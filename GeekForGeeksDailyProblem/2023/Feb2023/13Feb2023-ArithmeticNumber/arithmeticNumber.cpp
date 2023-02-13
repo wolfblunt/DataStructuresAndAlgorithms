ca05a2c4a173be1da32034f597e7bcbcff29019f
wolfblunt <amankhandelwaljuly@gmail.com>
2023-02-13 15:57:58 +0530

    13-2-2023 commit

---
+++ GeekForGeeksDailyProblem/2023/Feb2023/13Feb2023-ArithmeticNumber/arithmeticNumber.cpp
@@ -0,0 +1,31 @@
+// Arithmetic Number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        if(A==B)
+            return 1;
+        if(C==0)
+            return 0;
+        if((B-A)%C == 0 && (B-A)/C>=0)
+            return 1;
+        return 0;
+    }
+};
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}

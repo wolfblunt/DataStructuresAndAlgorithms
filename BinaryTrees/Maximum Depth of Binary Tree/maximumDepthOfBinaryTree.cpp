fdcda23cd37e6a27909dc03621fda0e404e0873d
wolfblunt <amankhandelwaljuly@gmail.com>
2023-02-16 23:45:07 +0530

    16Feb2023 commit

---
+++ BinaryTrees/Maximum Depth of Binary Tree/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,25 @@
+// Max Depth
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+
+        int leftLength = maxDepth(root->left);
+        int rightLength = maxDepth(root->right);
+        return max(leftLength, rightLength) + 1;
+    }
+};
+
